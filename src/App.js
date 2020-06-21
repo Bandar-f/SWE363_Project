dfe5c7f880857683cf2c7f87ec80291509c09e1c
Yasser A-J <yasserjaber19980@gmail.com>
2020-06-21 14:48:02 +0300

    added component of profile pic + name + rating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Form from './components/textFieldComponent/textField'
+import PersonWithRating from './components/PersonWithRating/PersonWithRating';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
-       
+      
     </div>
   );
 }
