4afa82fc568459598d601ab8e3a88ebdde801b79
Nawaf B Al sharqi <nawafbader@MacBook-Pro.local>
2020-06-21 13:04:01 +0300

    feature: finshing the first component FromCopmonent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Form from './components/textFieldComponent/textField'
+import Form from './components/FormComponent/Form'
 
 function App() {
   return (
