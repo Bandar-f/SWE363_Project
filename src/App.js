b294d75dfe2d288d9672484b2457c5026c2e5369
Nawaf B Al sharqi <nawafbader@MacBook-Pro.local>
2020-06-22 12:49:05 +0300

    feature: wide button component is done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import './components/Form';
-import Form from './components/Form';
 import Sidemenu from './components/Sidemenu/Sidemenu';
+import WideButton from './components/WideButtonComponent/WideButton';
 
 function App() {
    const titles=["History","Pickup Details", "Request ride"];
   return (
-    <div className="App">
-      <Sidemenu titles={titles}/>
+    <div  className="App">
+      <WideButton/>
       
       
     </div>
