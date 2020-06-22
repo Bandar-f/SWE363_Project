6d2caa263fd3accaea889c1591ca059767616d6f
a <bandarfahdd@gmail.com>
2020-06-22 18:57:03 +0300

    fixing naming of some component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Sidemenu from './components/Sidemenu/Sidemenu';
-<<<<<<< HEAD
-import CarComponent from "./components/CarComponent/CarComponent"
-=======
 import WideButton from './components/WideButtonComponent/WideButton';
 import DatePicker from './components/DatePickerComponent/DatePicker';
 import TimePicker from './components/TimePickerComponent/TimePicker';
@@ -11,20 +8,12 @@ import PhoneNum from './components/phoneNumComponent/PhoneNum';
 import FloatingLogo from './components/floatingLogoComponent/FloatingLogo';
 import WelcomePage from './components/WelcomePage/WelcomePage';
 import NavBar from './components/navBarComponent/NavBar';
->>>>>>> 713ddb0c47336ce91a8637df23b91f54005d5a77
 
 function App() {
    const titles=["History","Pickup Details", "Request ride"];
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <CarComponent/>
-     
-      
-=======
     <div  className="App">
       <WelcomePage/>
->>>>>>> 713ddb0c47336ce91a8637df23b91f54005d5a77
       
     </div>
   );
