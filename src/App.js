4331da64fb81c5d691ed5fa1386b131c7238281e
Nawaf B Al sharqi <nawafbader@MacBook-Pro.local>
2020-06-22 23:27:00 +0300

    feature package track component and including new npm dependicy that generates unique ids uiid()

--- src/App.js
+++ src/App.js
@@ -7,27 +7,15 @@ import TimePicker from './components/TimePickerComponent/TimePicker';
 import PhoneNum from './components/phoneNumComponent/PhoneNum';
 import FloatingLogo from './components/FloatingLogoComponent/FloatingLogo';
 import WelcomePage from './components/WelcomePage/WelcomePage';
-<<<<<<< HEAD
 import NavBar from './components/navBarComponent/navBar';
 import PackageTrack from './components/PackageTrackComponent/PackageTrack';
-=======
-import NavBar from './components/navBarComponent/NavBar';
-import RadioImgBackground from './components/RadioImgBackgroundComponent/RadioImgBackground'
-import Cap from './components/CarAndPerson/Cap'
->>>>>>> dcc7a240ea191b0c4af325251530b8cd41cc5c67
 
 //  
 function App() {
    const titles=["History","Pickup Details", "Request ride"];
   return (
     <div  className="App">
-<<<<<<< HEAD
      <WelcomePage/>
-=======
-      <WelcomePage/>
-
-      <Cap/>
->>>>>>> dcc7a240ea191b0c4af325251530b8cd41cc5c67
       
     </div>
   );
