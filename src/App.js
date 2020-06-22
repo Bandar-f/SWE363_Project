ba762dedfee9ff5070c3b3b515a9dd69c9557eff
Nawaf B Al sharqi <nawafbader@MacBook-Pro.local>
2020-06-22 13:18:23 +0300

    date picker components is ready

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Sidemenu from './components/Sidemenu/Sidemenu';
 import WideButton from './components/WideButtonComponent/WideButton';
+import DatePicker from './components/DatePickerComponent/DatePicker';
 
 function App() {
    const titles=["History","Pickup Details", "Request ride"];
   return (
     <div  className="App">
-      <WideButton/>
-      
+
+      <DatePicker/>
       
     </div>
   );
