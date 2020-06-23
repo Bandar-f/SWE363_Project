e9bdc2fae6597aa0daf49441ab202a96ab0cd6a5
Tariq <mrtariqss@outlook.com>
2020-06-23 14:40:11 +0300

    finally fixed capitalization for navbar and floatingLogo

---
+++ src/components/navBarComponent/NavBar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './navBar.css';
+import { render } from '@testing-library/react';
+
+                            /*----- TTK -----*/
+class NavBar extends Component{
+    render() {
+    return (
+        <div id = "NavBar">
+            <img id="SideMenu" src="https://www.hotel-igelheim.eu/img/mobile_nav_icon.png" alt="Side Menu"/>
+            <h1>{this.props.header}</h1> {/* allows for a variable page navigation information title, apply by initiating <NavBar header="Any Page Title"> */}
+            <img id="Notif" src="https://www.iconsdb.com/icons/preview/white/bell-2-xxl.png" alt="Notifications"/>
+        </div>
+    )
+    }
+}
+
+export default NavBar;
