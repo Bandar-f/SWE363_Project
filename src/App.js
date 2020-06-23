196b4a74f19eab9ea9524414cbeb0fb0f1edbbd7
Tariq <mrtariqss@outlook.com>
2020-06-23 22:27:46 +0300

    major alignment fixes and added a date and driver page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Sidemenu from './components/Sidemenu/Sidemenu';
 import WideButton from './components/WideButtonComponent/WideButton';
 import DatePicker from './components/DatePickerComponent/DatePicker';
 import TimePicker from './components/TimePickerComponent/TimePicker';
-import PhoneNum from './components/phoneNumComponent/PhoneNum';
+import PhoneNum from './components/PhoneNumComponent/PhoneNum';
 import Customer from './components/CustomerComponent/Customer'
 import PackageTrack from './components/PackageTrackComponent/PackageTrack';
 import Cap from './components/CarAndPerson/Cap';
-import Ut from './components/UpcomingTripsComponent/Ut';
+import Ut from './components/UpcomingTripsComponent/Ut'
+import RequestRide from './Pages/RequestRidePage/RequestRide';
+import SelectService from './components/SelectServiceComponent/SelectService';
 import LoginPage from './Pages/LoginPage/LoginPage';
 
 
--- src/components/PersonWithRatingComponent/PersonWithRating.js
+++ src/components/PersonWithRatingComponent/PersonWithRating.js
@@ -4,8 +4,8 @@ import './PersonWithRating.css'
 function PersonWithRating(props){
         
             return (
-                <div className="main">
-                  <img width="50" height="50" className="image" src="https://www.logolynx.com/images/logolynx/s_0d/0d63ec3fb980f07166db66e9ba273baa.jpeg" alt="personal pic"/>
+                <div>
+                  <img className="image" src="https://www.logolynx.com/images/logolynx/s_0d/0d63ec3fb980f07166db66e9ba273baa.jpeg" alt="personal pic"/>
                   <div className="text">
                   <p>Bandar albalawi</p>
                   <p>4.5/5</p>
