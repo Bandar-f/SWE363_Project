ae58563083bd809a5de0c3a3b30165f18fd107e1
Tariq <mrtariqss@outlook.com>
2020-06-23 17:19:07 +0300

    temporarily removed 3 files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import Sidemenu from './components/Sidemenu/Sidemenu';
 import WideButton from './components/WideButtonComponent/WideButton';
 import DatePicker from './components/DatePickerComponent/DatePicker';
 import TimePicker from './components/TimePickerComponent/TimePicker';
+<<<<<<< HEAD
 import PhoneNum from './components/PhoneNumComponent/PhoneNum';
 import NavBar from './components/NavBarComponent/NavBar'
 import FloatingLogo from './components/FloatingLogoComponent/FloatingLogo';
 
+=======
+import PhoneNum from './components/phoneNumComponent/PhoneNum';
+import NavBar from './components/navBarComponent/NavBar'
+import FloatingLogo from './components/floatingLogoComponent/FloatingLogo';
+import RadioImgBackground from './components/RadioImgBackgroundComponent/RadioImgBackground';
+>>>>>>> f02bd7ec7d17fb03329ab39760bcf03637576a18
 import CarComponent from './components/CarComponent/CarComponent';
 import Customer from './components/CustomerComponent/Customer'
 
@@ -22,6 +29,7 @@ function App() {
   return (
     <div  className="App">
             <Customer/>
+            
 
 
 
