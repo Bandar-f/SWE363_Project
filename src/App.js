de86b837e527867c6876601155e20c55ca75a2d7
Tariq <mrtariqss@outlook.com>
2020-06-23 14:34:21 +0300

    any input for the header in navbar will be capitalized

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import WideButton from './components/WideButtonComponent/WideButton';
 import DatePicker from './components/DatePickerComponent/DatePicker';
 import TimePicker from './components/TimePickerComponent/TimePicker';
 import PhoneNum from './components/phoneNumComponent/PhoneNum';
+import FloatingLogo from './components/floatingLogoComponent/FloatingLogo';
 
 import WelcomePage from './components/WelcomePage/WelcomePage';
+import CarComponent from './components/CarComponent/CarComponent';
 import Customer from './components/CustomerComponent/Customer'
 
 import PackageTrack from './components/PackageTrackComponent/PackageTrack';
 import Cap from './components/CarAndPerson/Cap';
 import Ut from './components/UpcomingTripsComponent/Ut'
+import NavBar from './components/NavBarComponent/NavBar';
 
-//  
+//
 function App() {
    const titles=["History","Pickup Details", "Request ride"];
   return (
     <div  className="App">
-      <Customer/>
+            <Customer/>
+            <NavBar header="is it cap?"/>
+      <WelcomePage/>
 
-      
 
-      
 
-      
-      
+
+
+
+
     </div>
   );
 }
--- src/components/navBarComponent/navBar.js
+++ src/components/navBarComponent/navBar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './NavBar.css';
+import './navBar.css';
 import { render } from '@testing-library/react';
 
                             /*----- TTK -----*/
@@ -8,7 +8,7 @@ class NavBar extends Component{
     return (
         <div id = "NavBar">
             <img id="SideMenu" src="https://www.hotel-igelheim.eu/img/mobile_nav_icon.png" alt="Side Menu"/>
-            <h1>{this.props.header}</h1>
+            <h1>{this.props.header}</h1> {/* allows for a variable page navigation information title, apply by initiating <NavBar header="Any Page Title"> */}
             <img id="Notif" src="https://www.iconsdb.com/icons/preview/white/bell-2-xxl.png" alt="Notifications"/>
         </div>
     )
