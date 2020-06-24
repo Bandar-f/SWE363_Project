63e7c35a45132de55052aad87eef95c33c24168b
Tariq <mrtariqss@outlook.com>
2020-06-24 15:23:08 +0300

    cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import RequestRide from './Pages/RequestRidePage/RequestRide'
 import DateNDriver from './Pages/RequestRidePage/RequestRide';
+import MoreDetailsPage from './Pages/MoreDetailsPage/MoreDetailsPage';
 
 
 //  
@@ -9,7 +10,7 @@ function App() {
    const titles=["History","Pickup Details", "Request ride"];
   return (
     <div  className="App">
-      <RequestRide/>
+      <MoreDetailsPage/>
     </div>
   );
 }
