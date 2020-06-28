d71926e5a530f7a408a28dc87d12eea69d1c8367
Tariq <mrtariqss@outlook.com>
2020-06-28 22:18:39 +0300

    completed blank buttons, added new button to schedule ride, and fixed alignment of schedule ride

--- src/Pages/ScheduleRide/ScheduleRide.js
+++ src/Pages/ScheduleRide/ScheduleRide.js
@@ -6,6 +6,8 @@ import Text from '../../components/TextComponent/Text';
 import DatePicker from '../../components/DatePickerComponent/DatePicker';
 import TimePicker from '../../components/TimePickerComponent/TimePicker';
 import DropDownAmount from '../../components/DropDownAmountComponent/DropDownAmount';
+import WideButton from '../../components/WideButtonComponent/WideButton';
+
 const ScheduleRide = () => {
   return (
     <motion.div initial={{opacity:0}}
@@ -15,19 +17,24 @@ const ScheduleRide = () => {
     <FloatingLogo/>
     <div className='all'>
     <div className='Date'>
+    <div className='pickupAlign'>
     <Text text='Select pickup date'/>
+    </div>
     <DatePicker/>
     </div>
     <div className='Time'>
+    <div className='timeAlign'>
     <Text text='Select pickup time'/>
+    </div>
     <TimePicker/>
     </div>
     <div className='Amnt'>
     <Text text='Passenger amount'/>
+    </div>
     <div className='DD'>
     <DropDownAmount/>
     </div>
-    </div>
+    <WideButton buttonTitle="Schedule Ride"/>
     </div>
     </motion.div>
   )
