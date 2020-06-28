68e6625cf236a0d08d4675d790b0697716744115
Yasser A-J <yasserjaber19980@gmail.com>
2020-06-28 21:27:23 +0300

    Modified DropDownAmount component + added schedule ride page

--- src/Pages/RequestRidePage/RequestRide.js
+++ src/Pages/RequestRidePage/RequestRide.js
@@ -38,11 +38,13 @@ function RequestRide() {
                 <option value="bqq">Albaqiq</option>
                 </select>
                 </div>
+                <div className='RRIB'>
                 <RadioImgBackground url1="https://audimediacenter-a.akamaihd.net/system/production/media/91178/images/1f201fd6f5fcbd78b452dd0ff4907b1cc4dc0a8c/A202506_blog.jpg?1587377988"
                 url2="https://i2.wp.com/us.motorsactu.com/wp-content/uploads/2019/10/Bentley-Flying_Spur-2020-1280-07.jpg?fit=1200%2C675&ssl=1"
                 Label1="Economy Tier" Label2="Luxury Tier"/>
                 <br/>
                 <Link to="/dateAndTime"> <WideButton buttonTitle="Continue"/> </Link>
+                </div>
             </section>
         </motion.div>
     )
--- src/Pages/ScheduleRide/ScheduleRide.js
+++ src/Pages/ScheduleRide/ScheduleRide.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import './scheduleRide.css'
+import React from 'react';
+import './scheduleRide.css';
 import {motion} from 'framer-motion';
-
-
+import FloatingLogo from '../../components/FloatingLogoComponent/FloatingLogo';
+import Text from '../../components/TextComponent/Text';
+import DatePicker from '../../components/DatePickerComponent/DatePicker';
+import TimePicker from '../../components/TimePickerComponent/TimePicker';
+import DropDownAmount from '../../components/DropDownAmountComponent/DropDownAmount';
 const ScheduleRide = () => {
   return (
     <motion.div initial={{opacity:0}}
         animate={{opacity:1}}
         exit={{opacity:0}}
-        transition={{duration:2}}
-         >
-      
-      <p className="testestt">Not done Yet</p>
-      
+        transition={{duration:2}}>
+    <FloatingLogo/>
+    <div className='all'>
+    <div className='Date'>
+    <Text text='Select pickup date'/>
+    <DatePicker/>
+    </div>
+    <div className='Time'>
+    <Text text='Select pickup time'/>
+    <TimePicker/>
+    </div>
+    <div className='Amnt'>
+    <Text text='Passenger amount'/>
+    <div className='DD'>
+    <DropDownAmount/>
+    </div>
+    </div>
+    </div>
     </motion.div>
   )
 }
