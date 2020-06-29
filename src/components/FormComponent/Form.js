5e0a21051e0cdefd316500f92f3252d30d9972ea
Tariq <mrtariqss@outlook.com>
2020-06-29 22:03:00 +0300

    gi

--- src/components/FormComponent/Form.js
+++ src/components/FormComponent/Form.js
@@ -10,15 +10,21 @@ export default function Form(props) {
 	return (
 		<div className="formContainer">
 			<form>
+				<div className='leftAlign'>
 				<Text text="Please enter your name:"/>
+				</div>
 				<input type="text" placeholder="name" />
 				<br></br>
+				<div className='leftAlign'>
 				{props.userType!="Customer"?<Text text="Please enter your Government ID:"/>:
 				""}
+				</div>
 				{props.userType!="Customer"?<input type="text" placeholder="ID" />:
 				""}
 				<br></br>
+				<div className='leftAlign'>
 				<Text text="Please enter your password:"/>
+				</div>
 				<input type="password" placeholder="Password" />
 				
 			
