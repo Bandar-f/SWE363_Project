f17a664c0ac47fb1db3a485adfd64c438979b88d
Tariq <mrtariqss@outlook.com>
2020-07-16 18:20:53 +0300

    reverted back the phoneNum changes

--- src/components/PhoneNumComponent/PhoneNum.js
+++ src/components/PhoneNumComponent/PhoneNum.js
@@ -3,23 +3,44 @@ import { Link } from 'react-router-dom';
 import './phoneNum.css';
 /*----- TTK -----*/
 const PhoneNum = (props) => {
+	const handelForm = (e) => {
+		props.formHandler(e);
+	};
 	return (
 		<div>
 			<select name="cc" id="countryCode" disabled={!props.isRadioClicked}>
-				<option name="cc" value="+966">+966</option>
-				<option name="cc" value="+965">+965</option>
-				<option name="cc" value="+973">+973</option>
-				<option name="cc" value="+971">+971</option>
-				<option name="cc" value="+974">+974</option>
+				<option name="cc" value="+966">
+					+966
+				</option>
+				<option name="cc" value="+965">
+					+965
+				</option>
+				<option name="cc" value="+973">
+					+973
+				</option>
+				<option name="cc" value="+971">
+					+971
+				</option>
+				<option name="cc" value="+974">
+					+974
+				</option>
 			</select>
-			<input type="tel" id="phone" name="phone" placeholder="5XXXXXXXX" disabled={!props.isRadioClicked} pattern="[0-9]{9}"></input>
+			<input
+				type="tel"
+				id="phone"
+				name="phone"
+				placeholder="5XXXXXXXX"
+				disabled={!props.isRadioClicked}
+				pattern="[0-9]{9}"
+				onChange={(e) => handelForm(e)}
+			></input>
 			<Link to="/secondLog">
-				<button id="nxtbtn" type="submit" disabled={!props.isRadioClicked}>
+				<button id="nxtbtn" type="submit" onClick={props.fetchUser} disabled={!props.isRadioClicked}>
 					<i className="fas fa-arrow-right"></i>
 				</button>
 			</Link>
 		</div>
 	);
-}
+};
 
 export default PhoneNum;
